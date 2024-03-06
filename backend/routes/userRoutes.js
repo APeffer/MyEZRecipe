19cddc2d61561c77e98ddce904e6220a6226e4a1
AyyPeff <log.alexpeffer@gmail.com>
2024-03-06 00:09:42 -0600

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,17 @@ const express = require('express');
 const { signupUser, loginUser } = require('../controllers/userController')
 const cors = require('cors')
 
-
-
 const router = express.Router();
 
-router.options('/login', cors());
+
+const corsOptions = {
+    origin: 'https://my-ez-recipe-frontend.vercel.app', // Replace with your frontend domain
+    methods: ['POST'], // Specify allowed methods
+    allowedHeaders: ['Content-Type'], // Specify allowed headers
+    optionsSuccessStatus: 200 // Specify the success status for preflight requests
+  };
+
+router.options('/login', cors(corsOptions));
 
 router.post('/signup', signupUser);
 
